70edb4907bbb45196ff5f6edca7930a637b10693
Jakub Kacprzak <jkacprzak263@gmail.com>
2024-01-01 15:32:34 +0100

    Implementring painting mechanics with random color generation

--- app.js
+++ app.js
@@ -9,9 +9,37 @@ function createBoard(boardSize){
     let boardElement = document.createElement("div");
     
     boardElement.setAttribute("class", "board-element");
+    boardElement.addEventListener("mouseenter", changeColor);
+    boardElement.addEventListener("mousedown", changeColor);
     
     fragment.appendChild(boardElement);
   }
   board.appendChild(fragment)
 }
+
 createBoard(boardSize);
+
+function getRandomColor(){
+  const hexPattern = ["A", "B", "C", "D", "E", "F"];
+
+  let randomHex = '';
+  let char;
+
+  for(let i = 0; i < 6; i++){
+    char = Math.floor(Math.random() * 15);
+    char > 9 ? randomHex += hexPattern[char - 10] : randomHex += char;;
+  }
+
+  return "#1c1c1c"
+  // return "#" + randomHex;
+}
+
+let mouseDown = false;
+
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
+
+function changeColor(e){
+  if(!mouseDown && e.type != "mousedown") return;
+  this.style.backgroundColor = getRandomColor();
+}
